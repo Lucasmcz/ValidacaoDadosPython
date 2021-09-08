22520e4853cf6a64f8bcaf6232102f0d959aeb0c
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-09-08 14:41:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cpf_cnpj import Documento
 
 
 exe = "35379838000112"
-exe_cpf = "44506596005"#5
+exe_cpf = "44506596005"
 
 documento = Documento.criar_documento(exe)
 documento2 = Documento.criar_documento(exe_cpf)
