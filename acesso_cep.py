9e1f16253eae870e608a8ec3f0969711774c8d89
Lucas Vinicius <88403508+Lucasmcz@users.noreply.github.com>
2021-09-08 17:40:12 -0300

    Add files via upload

---
+++ acesso_cep.py
@@ -0,0 +1,32 @@
+import requests#Chamar API IMPORTANTE PRA KRL!!!
+class BuscaEndereco:
+
+    def __init__(self,cep):
+        cep = str(cep)
+        if(self.cep_e_valido(cep)):
+            self.cep = cep
+        else:
+            raise ValueError("CEP Invalido")
+
+    def cep_e_valido(self,cep):
+        if(len(cep) == 8):
+            return True
+        else:
+            return False
+
+    def format_cep(self):
+        return "{}-{}".format(self.cep[:5],self.cep[5:])
+    def __str__(self):
+        return self.acessa_viacep()
+
+    def acessa_viacep(self):
+        url = "https://viacep.com.br/ws/{}/json".format(self.cep)#Url de todas API do ViaCEP
+        r = requests.get(url)
+        dados = r.json()#metodo JSON(Dict)
+        return(
+            dados['bairro'],
+            dados['localidade'],
+            dados['uf'],
+
+
+        )
